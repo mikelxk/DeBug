b96fe09c68b007a55f444e4e8935dcc9ad44e080
Mik3L1 <mike.lxk@gmail.com>
2020-02-01 15:54:40 -0500

    list done!

--- app/src/main/java/com/hack/debug/MainActivity.java
+++ app/src/main/java/com/hack/debug/MainActivity.java
@@ -3,13 +3,27 @@ package com.hack.debug;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.hack.debug.ui.home.HomeViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    ListView simpleList;
+    List list = new ArrayList();
+    ArrayAdapter adapter;
+    String countryList[] = {"India", "China", "australia", "Portugle", "America", "NewZealand"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +38,36 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+
+        listView = (ListView)findViewById(R.id.showList);
+        testAddList();//test add a item
+        adapter= new ArrayAdapter(this,android.R.layout.simple_list_item_1,list);
+        listView.setAdapter(adapter);
+    }
+//    public void addContent(ArrayList addList){
+//        list.addAll(addList);
+//    }
+    public boolean testAddList(){
+        list.add("orange");
+        list.add("orange");
+        list.add("orange");
+        list.add("orange");
+        list.add("orange");
+        list.add("orange");
+        list.add("orange");
+        list.add("orange");
+        list.add("orange");
+        list.add("orange");
+        list.add("orange");
+        list.add("orange");
+        list.add("orange");
+        list.add("orange");
+        list.add("orange");
+        list.add("orange");
+        list.add("orange");
+        list.add("orange");
+        return true;
     }
 
 }
