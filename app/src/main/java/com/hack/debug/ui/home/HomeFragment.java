42d70a089215a8d1ae8f7d26617b25c7829555a9
Mik3L1 <mike.lxk@gmail.com>
2020-02-02 01:10:20 -0500

    change format

--- app/src/main/java/com/hack/debug/ui/home/HomeFragment.java
+++ app/src/main/java/com/hack/debug/ui/home/HomeFragment.java
@@ -15,21 +15,38 @@ import android.support.v4.app.Fragment;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 
+
+import com.google.android.gms.maps.MapsInitializer;
 import com.hack.debug.Main2Activity;
+
+import com.google.android.gms.maps.SupportMapFragment;
+
 import com.hack.debug.R;
 
 import java.util.ArrayList;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
 
-public class HomeFragment extends Fragment {
+
+public class HomeFragment extends Fragment implements OnMapReadyCallback{
 
     private HomeViewModel homeViewModel;
+    GoogleMap mGooglemap;
+    MapView mMapview;
+    View view;
     public ArrayList<String> myList = new ArrayList<String>();
     public HomeFragment(){
     }
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater,
+    public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        view = inflater.inflate(R.layout.fragment_home, container, false);
         ListView listView = (ListView) view.findViewById(R.id.showList);
 
         listAddList(new ArrayList<String>(){
@@ -47,6 +64,21 @@ public class HomeFragment extends Fragment {
         listView.setAdapter(liveViewAdapter);
 
         return view;
+    }
+    @Override
+    public void onViewCreated(View view,Bundle savedInstanceState){
+        super.onViewCreated(view,savedInstanceState);
+        mMapview=(MapView)view.findViewById(R.id.mapView);
+        if(mMapview!=null){
+            mMapview.onCreate(null);
+            mMapview.onResume();
+            mMapview.getMapAsync(this);
+        }
+    }
+    public void onMapReady(GoogleMap googleMap){
+        MapsInitializer.initialize(getContext());
+        mGooglemap = googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
     }
     public void listAdd(String s){
         myList.add(s);
